21c223014800cd04e74328f63587179e2032d010
Matthew Rho <mcr9754@rit.edu>
2022-06-01 03:39:33 -0400

    switch to catch commands

--- Program.cs
+++ Program.cs
@@ -1,9 +1,44 @@
 ﻿using System;
 
-namespace hw {
+namespace Project {
     class Program {
+
+        /*
+         * Parses a command and performs some logic.
+         *
+         * return false if quitting, true otherwise
+         */
+        public static bool ParseCmd(string cmd) {
+            switch(cmd) {
+                case "ascii":
+                    return true;
+                case "binary":
+                    return true;
+                case "cd":
+                    return true;
+                case "cdup":
+                    return true;
+                case "debug":
+                    return true;
+                case "dir":
+                    return true;
+                case "get":
+                    return true;
+                case "help":
+                    return true;
+                case "passive":
+                    return true;
+                case "pwd":
+                    return true;
+                case "quit":
+                    return false;
+            }
+
+            return true;
+        }
+
         public static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
